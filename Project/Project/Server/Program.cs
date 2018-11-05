4379e1c3853296d7fce38b4473d15c1b5c214be8
IrinaJovanovic <39349895+IrinaJovanovic@users.noreply.github.com>
2018-11-05 20:38:06 +0100

    prvi_deo_ok
    Zavrsen prvi deo projekta

---
+++ Project/Project/Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+            ServiceHost svc = new ServiceHost(typeof(DataBaseManagement));
+            svc.AddServiceEndpoint(typeof(IDatabaseManagement), new NetTcpBinding(), new Uri("net.tcp://localhost:4001/IDataBaseManagement"));
+
+           
+            svc.Open();
+           
+            Console.WriteLine("Server je otvoren.");
+            Console.ReadLine();
+            svc.Close();
+        }
+
+        
+    }
+}
